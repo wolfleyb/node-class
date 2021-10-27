8e1e16c921ad353edc5319d29fc4e8c6a351436c
usacys <usacys@local>
2021-10-27 15:50:08 -0400

    with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,34 +1,45 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+// Setup handlebars engine and views location
 app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup folder for static views
 app.use(express.static(publicDirectoryPath))
+
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Andrew Mead',
-
+        name: 'Ben Wolfley',
     })
+})
 
 app.get('/about', (req, res) => {
-    res.render('about' {
+    res.render('about', {
         title: 'About Me',
         name: 'Ben Wolfley'
     })
 })
 
 app.get('/help', (req, res) => {
-
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
 })
 
-})
 
 app.get('/weather', (req, res) => {
     res.send({
